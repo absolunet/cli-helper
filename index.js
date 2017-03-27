dfa8649f34ae85aaab923427b82905e6b658b64d
Jb Landry <jb@jblandry.info>
2017-03-27 10:17:40 -0400

    Tasks management

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const echo = console.log; // eslint-disable-line no-console
 
 const os          = require('os');
 const path        = require('path');
+const glob        = require('glob');
 const readPkgUp   = require('read-pkg-up');
 const chalk       = require('chalk');
 const pad         = require('pad');
@@ -27,7 +28,11 @@ const STATIC = global.___AbsolunetCli___ ? global.___AbsolunetCli___ : global.__
 	commands:  {},
 	taskWidth: {},
 	fullUsage: {},
-	baseWidth: 0
+	baseWidth: 0,
+	tasks:     {
+		path: '',
+		list: []
+	}
 };
 
 
@@ -145,6 +150,46 @@ module.exports = class Cli {
 		if (meowCli.input.length > 1 || Object.keys(meowCli.flags).length) {
 			this.showTaskUsage(meowCli);
 		}
+	}
+
+
+
+
+
+
+	//-- List tasks files
+	static initTasksList(tasksPath) {
+		STATIC.tasks.path = tasksPath;
+
+		const tasks = [];
+
+		glob.sync(`${STATIC.tasks.path}/!(default).js`).forEach((task) => {
+			tasks.push(task.split(STATIC.tasks.path).slice(-1).pop().substring(1).slice(0, -3));
+		});
+
+		STATIC.tasks.list = tasks;
+	}
+
+
+	//-- Route to good task
+	static tasksRouter(meowCli) {
+		const [task] = meowCli.input;
+
+		if (task) {
+			if (STATIC.tasks.list.includes(task)) {
+				require(`${STATIC.tasks.path}/${task}`)(meowCli);  // eslint-disable-line global-require
+			} else {
+				meowCli.showHelp();
+			}
+		} else {
+			require(`${STATIC.tasks.path}/default`)(meowCli);  // eslint-disable-line global-require
+		}
+	}
+
+
+	//-- Tasks
+	static get tasks() {
+		return STATIC.tasks.list;
 	}
 
 
